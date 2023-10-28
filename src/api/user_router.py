67b7fa7e2832475db05dc48ceede12fae3646a70
Raymond Shum <74946530+raymondshum@users.noreply.github.com>
2023-10-28 15:16:11 -0700

    DEV: Moved user routes to api folder

---
+++ src/api/user_router.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import EmailStr
+
+from model.schema import UserCreate, UserResponse, UserUpdate
+from service.database import SessionLocal, get_db
+from service.user_service import UserService
+
+router = APIRouter(prefix="/v1/user")
+
+
+@router.post("/user", response_model=UserResponse)
+def create_user(user: UserCreate, db: SessionLocal = Depends(get_db)):
+    existing_user = UserService.get_user_by_email(db=db, email=user.email)
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return UserService.create_user(db=db, user=user)
+
+
+@router.get("/user/{email}", response_model=UserResponse)
+def get_user(email: EmailStr, db: SessionLocal = Depends(get_db)):
+    existing_user = UserService.get_user_by_email(db=db, email=email)
+    if not existing_user:
+        raise HTTPException(
+            status_code=404, detail=f"User not found for email: {email}"
+        )
+    return existing_user
+
+
+@router.delete("/user/{email}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(email: EmailStr, db: SessionLocal = Depends(get_db)):
+    existing_user = UserService.get_user_by_email(db=db, email=email)
+    if not existing_user:
+        raise HTTPException(
+            status_code=404, detail=f"User not found for email: {email}"
+        )
+    existing_user = UserService.delete_user(db=db, user=existing_user)
+
+
+@router.put("/user/{email}", response_model=UserResponse)
+def update_user(
+    email: EmailStr, updated_fields: UserUpdate, db: SessionLocal = Depends(get_db)
+):
+    existing_user = UserService.get_user_by_email(db=db, email=email)
+    if not existing_user:
+        raise HTTPException(
+            status_code=404, detail=f"User not found for email: {email}"
+        )
+    return UserService.update_user(
+        db=db, updated_fields=updated_fields, existing_user=existing_user
+    )
--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
 import logging
 
 import uvicorn
-from fastapi import Depends, FastAPI, HTTPException, status
-from pydantic import EmailStr
+from fastapi import FastAPI
 
-from model.schema import UserCreate, UserResponse, UserUpdate
-from service.database import SessionLocal, get_db
-from service.user_service import UserService
+from api.user_router import router as user_router
 
 # TODO: Implement uvicorn logging
 logger = logging.getLogger(__name__)
@@ -16,53 +13,12 @@ logging.basicConfig(
 )
 
 app = FastAPI()
+app.include_router(user_router)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.post("/user", response_model=UserResponse)
-def create_user(user: UserCreate, db: SessionLocal = Depends(get_db)):
-    existing_user = UserService.get_user_by_email(db=db, email=user.email)
-    if existing_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return UserService.create_user(db=db, user=user)
-
-
-@app.get("/user/{email}", response_model=UserResponse)
-def get_user(email: EmailStr, db: SessionLocal = Depends(get_db)):
-    existing_user = UserService.get_user_by_email(db=db, email=email)
-    if not existing_user:
-        raise HTTPException(
-            status_code=404, detail=f"User not found for email: {email}"
-        )
-    return existing_user
-
-
-@app.delete("/user/{email}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(email: EmailStr, db: SessionLocal = Depends(get_db)):
-    existing_user = UserService.get_user_by_email(db=db, email=email)
-    if not existing_user:
-        raise HTTPException(
-            status_code=404, detail=f"User not found for email: {email}"
-        )
-    existing_user = UserService.delete_user(db=db, user=existing_user)
-
-
-@app.put("/user/{email}", response_model=UserResponse)
-def update_user(
-    email: EmailStr, updated_fields: UserUpdate, db: SessionLocal = Depends(get_db)
-):
-    existing_user = UserService.get_user_by_email(db=db, email=email)
-    if not existing_user:
-        raise HTTPException(
-            status_code=404, detail=f"User not found for email: {email}"
-        )
-    return UserService.update_user(
-        db=db, updated_fields=updated_fields, existing_user=existing_user
-    )
 
 
 if __name__ == "__main__":
