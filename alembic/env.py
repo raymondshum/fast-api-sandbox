446a89b26d3c6e3be652b8878eb398393f8a29b6
Raymond Shum <74946530+raymondshum@users.noreply.github.com>
2023-10-28 14:45:44 -0700

    CONFIG: Set up USER table

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,6 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 
@@ -17,7 +16,7 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-from src.model.base import Base
+from src.model.model import Base
 
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
